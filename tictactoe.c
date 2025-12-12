e76b18429958df70e91d8847d9e4e1e5e7df38ca
enkoki <reasondahal719@gmail.com>
2025-12-13 02:36:43 +0545

    Code fully written, havent debugged

--- tictactoe.c
+++ tictactoe.c
@@ -1,5 +1,7 @@
 // Player VS Computer
 
+// MSYS
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -19,9 +21,9 @@ void printWinner(char winner);
 void printBoard(){
     printf(" %c | %c | %c \n",board[0][0], board[0][1],board[0][2]);
     printf("---|---|---\n");
-    printf(" %c | %c | %c \n",board[1][0], board[1][1],board[1][2]);
+    printf(" %c| %c| %c\n",board[1][0], board[1][1],board[1][2]);
     printf("---|---|---\n");
-    printf(" %c | %c | %c \n",board[2][0], board[2][1],board[2][2]);
+    printf(" %c| %c| %c\n",board[2][0], board[2][1],board[2][2]);
     printf("---|---|---\n\n");
 }
 void resetBoard(){
